58661c46861d173a8105594d1582d5eb62639893
edward cadet <e_cadet22@hotmail.com>
2020-06-23 16:55:01 -0400

    git init, layout V1 done

---
+++ app/src/main/java/com/example/gestionfactureessence/AffichageActivity.java
@@ -0,0 +1,14 @@
+package com.example.gestionfactureessence;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AffichageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_affichage);
+    }
+}
---
+++ app/src/main/java/com/example/gestionfactureessence/RechercheActivity.java
@@ -0,0 +1,14 @@
+package com.example.gestionfactureessence;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RechercheActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recherche);
+    }
+}
